ee9fdbf4799a90803f0c3caf709483b7e9c5ed1e
ryaaahs <reily.maahs@gmail.com>
2025-04-26 20:45:40 -0400

    Init commit

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+/* Hello world program */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;    // Holds signed integers, e.g. -3, -2, 0, 1, 10
+    float f;  // Holds signed floating point numbers, e.g. -3.1416
+    
+    printf("Hello, World!\n");  // Actually do the work here
+}
